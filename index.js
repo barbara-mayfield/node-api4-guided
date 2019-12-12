2b2c90758eea2321ec24c3123b7627181380b69e
Barbara Mayfield <bmayfield1991@gmail.com>
2019-12-11 20:14:56 -0600

    Updated code to match Jason's

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express")
-// const dotenv = require("dotenv")
-//
-// dotenv.config()
 
+// Could do this instead of -r dotenv/config in the package.json
+// but since .env isn't uploaded to the repo, it's safer to not try
+// to look for the file in production.]
+//
+// const dotenv = require("dotenv")
+// 
+// dotenv.config()
 
 const app = express()
 const host = process.env.HOST || "0.0.0.0"
@@ -16,9 +20,11 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
 	res.json({
 		message: "Welcome to our API",
-		cohort: provess.env.LAMBDA_COHORT,
-		// secret:"hfaiwoehfo94032u4hnkljhbf"
-		secret: process.env.SUPER_SECRET_API_KEY
+		cohort: process.env.LAMBDA_COHORT,
+		// this value is now extracted into our environment variable,
+		// so we can keep it out of our code and our repository
+		// secret: "hfaiwoehfo94032u4hnkljhbf"
+		secret: process.env.SUPER_SECRET_API_KEY,
 	})
 })
 
